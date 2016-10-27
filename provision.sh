b8f1503f2c301e272c7653a30550e17d7b2172cd
Atanas Mirchev <atanas.mirchev@est.fujitsu.com>
2016-10-27 10:17:59 +0200

    Update provisioning scripts

--- provision.sh
+++ provision.sh
@@ -18,6 +18,18 @@ curl -s -L https://storage.googleapis.com/kubeadm/kubernetes-xenial-preview-bund
 sudo dpkg -i kubernetes-xenial-preview-bundle/*.deb
 
 # hack for openstack
-sudo sed -i -e 's#KUBELET_EXTRA_ARGS=#KUBELET_EXTRA_ARGS=--cloud-config=/home/ubuntu/heat-config.ini --cloud-provider=openstack #g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+sudo sed -i -e 's#KUBELET_EXTRA_ARGS=#KUBELET_EXTRA_ARGS=--cloud-config=/etc/kubernetes/cloud-config.json --cloud-provider=openstack #g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+cat <<EOF > /etc/kubernetes/cloud-config.json
+[global]
+auth-url=http://172.31.0.101:5000/v2.0
+username=mirchev
+password=secret
+tenant-id=91908fb11f60410eb2574d9fcea3fd52
+tenant-name=k8s-autoscale
+# domain-id=default
+# domain-name=Default
+EOF
+
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
